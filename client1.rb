c3d720267c45bab2f6abdd1c25a66c277cc14ee4
Vitor Carminatti <vitor.carminatti@hotmail.com>
2019-11-11 09:01:13 -0300

    Adicionado chamada a api para pegar hora

--- client1.rb
+++ client1.rb
@@ -1,3 +1,16 @@
+def getHour()
+  require 'net/http'
+  require 'date'
+
+  uri = URI("http://worldtimeapi.org/api/timezone/America/Sao_Paulo")
+
+  res = Net::HTTP.get_response(uri)
+
+  data = DateTime.parse(JSON.parse(res.body)["datetime"])
+
+  data.hour
+end
+
 print "Client node 1 \n"
 
 require "socket"
@@ -7,6 +20,6 @@ ts = 0
 
 loop do
   sleep(rand(5))
-  ts+=5
+  ts+=getHour()
   TCPSocket.open("localhost", 2000).puts(JSON.generate({action: 'insert', ts: ts}))
 end
--- client3.rb
+++ client3.rb
@@ -1,3 +1,16 @@
+def getHour()
+  require 'net/http'
+  require 'date'
+
+  uri = URI("http://worldtimeapi.org/api/timezone/Europe/Moscow")
+
+  res = Net::HTTP.get_response(uri)
+
+  data = DateTime.parse(JSON.parse(res.body)["datetime"])
+
+  data.hour
+end
+
 print "Client node 3 \n"
 
 require "socket"
@@ -7,6 +20,6 @@ ts = 0
 
 loop do
   sleep(rand(5))
-  ts+=20
+  ts+=getHour()
   TCPSocket.open("localhost", 2000).puts(JSON.generate({action: 'delete', ts: ts}))
 end
